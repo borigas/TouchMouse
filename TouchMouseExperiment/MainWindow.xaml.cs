5acf623dff5841f3f3178018421f334ae6ca5afc
Ben Origas <borigas@gmail.com>
2012-01-28 03:37:14 -0600

    Uses timer to check byte[]

--- TouchMouseExperiment/MainWindow.xaml.cs
+++ TouchMouseExperiment/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.Research.TouchMouseSensor;
 using System.Diagnostics;
+using System.Threading;
+using System.Windows.Threading;
 
 namespace TouchMouseExperiment
 {
@@ -30,6 +32,11 @@ namespace TouchMouseExperiment
 
             // Initialise the mouse and register the callback function
             TouchMouseSensorEventManager.Handler += TouchMouseSensorHandler;
+
+            _timer = new DispatcherTimer();
+            _timer.Tick += new EventHandler(TimerTick);
+            _timer.Interval = new TimeSpan(0, 0, 0, 0, ElapsedThreshold);
+            _timer.Start();
         }
 
         /// <summary>
@@ -44,183 +51,57 @@ namespace TouchMouseExperiment
             Dispatcher.Invoke((Action<TouchMouseSensorEventArgs>)SetSource, e);
         }
 
+        private DispatcherTimer _timer = null;
         List<TouchImage> TouchImages = new List<TouchImage>();
-        int ElapsedMillis = 0;
         const int ElapsedThreshold = 100;
+        private TouchMouseSensorEventArgs _args = null;
 
         void SetSource(TouchMouseSensorEventArgs e)
         {
-            if (ElapsedMillis > ElapsedThreshold)
+            _args = e;
+        }
+
+        private void TimerTick(object state, EventArgs e)
+        {
+            if (_args == null)
+                return;
+            
+            TouchImage ti = new TouchImage()
             {
-                // Convert bitmap from memory to graphic form.
-                //BitmapSource source =
-                //    BitmapSource.Create(e.Status.m_dwImageWidth, e.Status.m_dwImageHeight,
-                //    96, 96,
-                //    PixelFormats.Gray8, null, e.Image, e.Status.m_dwImageWidth);
+                Height = (byte)_args.Status.m_dwImageHeight,
+                Width = (byte)_args.Status.m_dwImageWidth,
+                Image = _args.Image,
+            };
 
-                //Bitmap
-                // Show bitmap in user interface.
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            ti.FindTouchPoints();
+            sw.Stop();
 
-                TouchImage ti = new TouchImage()
-                {
-                    Height = (byte)e.Status.m_dwImageHeight,
-                    Width = (byte)e.Status.m_dwImageWidth,
-                    Image = e.Image,
-                };
-
-                Stopwatch sw = new Stopwatch();
-                sw.Start();
-                ti.FindTouchPoints();
-                sw.Stop();
-
-                if (ti.TouchPoints.Count > 0)
-                {
-                    TouchImages.Add(ti);
-                }
-                else
-                {
-                    TouchImages.Clear();
-                }
-
-                System.Diagnostics.Trace.WriteLine("Elapsed: " + sw.ElapsedMilliseconds + ", Between Events: " + e.Status.m_dwTimeDelta);
-                foreach (var point in ti.TouchPoints)
-                {
-                    System.Diagnostics.Trace.WriteLine(string.Format("{0}, {1}: {2}", point.FocalPointX, point.FocalPointY, point.FocalPointValue));
-                }
-                System.Diagnostics.Trace.WriteLine("--------------------------");
-
-                //SensorImage.Source = ti.GetSensorImage();
-                //SensorImage.Source = ti.GetTouchPointImage();
-                SensorImage.Source = ti.GetTouchPointImageColored();
-
-                ElapsedMillis = 0;
+            if (ti.TouchPoints.Count > 0)
+            {
+                TouchImages.Add(ti);
             }
             else
             {
-                ElapsedMillis += e.Status.m_dwTimeDelta;
-            }
-        }
+                // Touch Gesture over
+                //if (TouchImages.Count > 0 && TouchImages.Count < 3)
+                //{
+                //    MouseHelper.LeftClick();
+                //}
 
-        private BitmapSource ProcessImage2(byte[] originalImage, int width, int height)
-        {
-            byte[] processedImage = new byte[originalImage.Length * 3];
-            int[] currentTouchPoint = null;
-            List<int[]> touchPoints = new List<int[]>();
-            for (int i = 0; i < originalImage.Length; i++)
-            {
-                processedImage[3 * i] = originalImage[i];
-
-                if (originalImage[i] > 0)
-                {
-                    if (!HasBeenChecked(originalImage, i % width, i / width, width))
-                    {
-                        currentTouchPoint = FindMainTouchPoint(originalImage, i % width, i / width, width, height);
-                        touchPoints.Add(currentTouchPoint);
-                        int touchPointIndex = GetByteIndex(currentTouchPoint[0], currentTouchPoint[1], width);
-                        processedImage[3 * touchPointIndex + 1] = Convert.ToByte(currentTouchPoint[2]);
-                    }
-                }
+                TouchImages.Clear();
             }
-            foreach (var points in touchPoints)
+
+            foreach (var point in ti.TouchPoints)
             {
-                System.Diagnostics.Trace.WriteLine(string.Format("{0}, {1}: {2}", points[0], points[1], points[2]));
+                System.Diagnostics.Trace.WriteLine(string.Format("{0}, {1}: {2}", point.FocalPointX, point.FocalPointY, point.FocalPointValue));
             }
             System.Diagnostics.Trace.WriteLine("--------------------------");
 
-            //return BitmapSource.Create(width, height, 96, 96,
-            //    PixelFormats.Indexed8, BitmapPalettes.Gray256, originalImage, width);
-
-            return BitmapSource.Create(width, height, 96, 96,
-                PixelFormats.Rgb24, null, processedImage, width * 3);
-        }
-
-        private int[] FindMainTouchPoint(byte[] image, int x, int y, int width, int height)
-        {
-            int left = x;
-            int right = x;
-            int top = y;
-            int bottom = y;
-
-            int currentX = x;
-            int currentY = y;
-
-            int maxX = x;
-            int maxY = y;
-            byte maxValue = image[GetByteIndex(currentX, currentY, width)];
-
-            while (HasValueBelow(image, width, height, currentX, currentY))
-            {
-                while (HasValueRight(image, width, currentX, currentY))
-                {
-                    currentX++;
-                    if (image[GetByteIndex(currentX, currentY, width)] > maxValue)
-                    {
-                        maxX = currentX;
-                        maxY = currentY;
-                        maxValue = image[GetByteIndex(currentX, currentY, width)];
-                        if (maxValue == Byte.MaxValue)
-                        {
-                            return new int[] { maxX, maxY, maxValue };
-                        }
-                    }
-                }
-                currentY++;
-            }
-            return new int[] { maxX, maxY, maxValue };
-        }
-
-        // Checks all 3 adjacent lower squares
-        private static bool HasValueBelow(byte[] image, int width, int height, int x, int y)
-        {
-            return y < height - 1 &&
-                (image[GetByteIndex(x, y + 1, width)] > 0
-                    || HasValueLeft(image, width, x, y + 1)
-                    || HasValueRight(image, width, x, y + 1));
-        }
-
-        private static bool HasValueRight(byte[] image, int width, int x, int y)
-        {
-            return x < width - 1 && image[GetByteIndex(x + 1, y, width)] > 0;
-        }
-
-        private static bool HasValueLeft(byte[] image, int width, int x, int y)
-        {
-            return x > 0 && image[GetByteIndex(x - 1, y, width)] > 0;
-        }
-
-        private static int GetByteIndex(int x, int y, int width)
-        {
-            return width * y + x;
-        }
-
-        private static bool HasBeenChecked(byte[] image, int x, int y, int width)
-        {
-            // If left pixel or 3 above pixels were > 0, this was already checked
-
-            // check left
-            if (x > 0 && y >= 0 && image[(x - 1) + y * width] > 0)
-            {
-                return true;
-            }
-            // check diagonal up and left
-            else if (x > 0 && y > 0 && image[(x - 1) + (y - 1) * width] > 0)
-            {
-                return true;
-            }
-            // check up
-            else if (x >= 0 && y > 0 && image[x + (y - 1) * width] > 0)
-            {
-                return true;
-            }
-            // check diagonal up and right
-            else if (x + 1 < width && y >= 0 && image[(x + 1) + (y - 1) * width] > 0)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
+            //SensorImage.Source = ti.GetSensorImage();
+            //SensorImage.Source = ti.GetTouchPointImage();
+            SensorImage.Source = ti.GetTouchPointImageColored();
         }
     }
 }
